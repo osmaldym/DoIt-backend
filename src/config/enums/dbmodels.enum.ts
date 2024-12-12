32161d2cfd6c19b78179b186ce19d8238eb0aed6
osmaldym <osmaldy11checo@gmail.com>
2024-12-11 20:51:16 -0400

    Creating enum for db models and Magic Strings

---
+++ src/config/enums/dbmodels.enum.ts
@@ -0,0 +1,11 @@
+export enum DBModel {
+    user = 'User',
+    task = 'Task',
+    category ='Category',
+}
+
+export enum MagicStrings {
+    USER = 'USER_MODEL',
+    TASK = 'TASK_MODEL',
+    CATEGORY = 'CATEGORY_MODEL',
+}
