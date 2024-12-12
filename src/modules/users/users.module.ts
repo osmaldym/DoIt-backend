9baac2c800e91c745ec029ccbac75cb29c4acda4
osmaldym <osmaldy11checo@gmail.com>
2024-12-11 20:55:07 -0400

    Creating user module

---
+++ src/modules/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { DbModule } from '../db/db.module';
+import { userProviders } from 'src/providers/user/user';
+
+@Module({
+  imports: [DbModule],
+  controllers: [UsersController],
+  providers: [UsersService, ...userProviders],
+})
+export class UsersModule {}
