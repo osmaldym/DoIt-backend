81d16f203116d6e07f53c8c2cb3a444bde9e3b8e
osmaldym <osmaldy11checo@gmail.com>
2024-12-14 01:31:07 -0400

    Creating guard for auth provider

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,42 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { catch_unwind } from "rusting-js";
+import { jwtConstants } from "src/config/constants";
+import { Guard } from "src/config/enums/guard.enum";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService, private reflector: Reflector) {}
+    
+    private extractTokenFromHeader(req: Request): string | undefined {
+        const [type, token] = req.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(Guard.GUARD_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass()
+        ])
+
+        if (isPublic) return true;
+
+        const req = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(req);
+        if (!token) throw new UnauthorizedException();
+
+        const res = await catch_unwind(async () => {
+            const payload = await this.jwtService.verifyAsync(token, { 
+                secret: jwtConstants.secret
+            })
+    
+            req['user'] = payload;
+        })
+
+        if (res.is_err()) throw new UnauthorizedException();
+
+        return true;
+    }
+}
