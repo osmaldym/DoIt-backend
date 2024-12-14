df7aa4ffcebcd739adff3bc1c25783c8bb2ef277
osmaldym <osmaldy11checo@gmail.com>
2024-12-14 01:50:53 -0400

    Adding auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, CategoriesModule],
+  imports: [UsersModule, CategoriesModule, TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
