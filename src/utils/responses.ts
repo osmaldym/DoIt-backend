4d97e4e03cc6c9d2c94790656ffa6adfecea57d8
osmaldym <osmaldy11checo@gmail.com>
2024-12-15 00:26:09 -0400

    Creating reponses util to return data by success or by rusting

---
+++ src/utils/responses.ts
@@ -0,0 +1,15 @@
+import { Result } from "rusting-js/enums";
+import { Success } from "./http/success";
+import { HttpException } from "@nestjs/common";
+
+export function success(data: any): Success {
+    return new Success(data);
+}
+
+/** 
+ * @todo Use best practices for rusting library
+ */
+export function rustingDepending(result: Result<Success, HttpException>): Success | HttpException {
+    if (result.is_err()) return result.unwrap_err();
+    return result.unwrap()
+}
