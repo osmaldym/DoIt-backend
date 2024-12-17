89587d1fffd863d09868442f7d99699166d9f923
osmaldym <osmaldy11checo@gmail.com>
2024-12-17 02:19:39 -0400

    Using deleteAts function in find methods

--- src/utils/calls.ts
+++ src/utils/calls.ts
@@ -1,12 +1,17 @@
 import mongoose, { Model } from "mongoose"
+import { deleteAts } from "./responses";
 
 export class DBCall {
-    static findOne(model: Model<any>, id: mongoose.Types.ObjectId) {
-        return model.findOne({ _id: id, deleted: false });
+    static async findOne(model: Model<any>, id: mongoose.Types.ObjectId) {
+        let data = await model.findOne({ _id: id, deleted: false });
+        data = deleteAts(data);
+        return data;
     }
 
-    static findAll(model: Model<any>) {
-        return model.find({ deleted: false }).exec();
+    static async findAll(model: Model<any>) {
+        let allData = await model.find({ deleted: false }).exec();
+        allData = allData.map(deleteAts);
+        return allData;
     }
 
     static updateOne(model: Model<any>, id: mongoose.Types.ObjectId, updateData: any){
