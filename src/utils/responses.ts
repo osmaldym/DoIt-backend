7d13b2c29fe54d6b8618b2c683bf2b2ca12cfdf0
osmaldym <osmaldy11checo@gmail.com>
2024-12-17 02:17:17 -0400

    Creating function deleteAts for remove all at property in mongoose doc

--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -1,6 +1,7 @@
 import { Result } from "rusting-js/enums";
 import { Success } from "./http/success";
 import { HttpException } from "@nestjs/common";
+import mongoose from "mongoose";
 
 export function success(data: any): Success {
     return new Success(data);
@@ -8,4 +9,13 @@ export function success(data: any): Success {
 
 export function rustingDepending(result: Result<Success, HttpException>): Success | HttpException {
     return result.unwrap_or_else((err) => { throw err });
+}
+
+export function deleteAts(el: mongoose.Document) {
+    let data = el.toJSON()
+    delete data.deletedAt;
+    delete data.createdAt;
+    delete data.updatedAt;
+    delete data.deleted;
+    return data;
 }
