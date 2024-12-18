a67e4456426764be8afd4cece113ab14949911fb
osmaldym <osmaldy11checo@gmail.com>
2024-12-18 02:03:35 -0400

    Importing AuthService and using getUser to find and create data

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -5,24 +5,27 @@ import { MagicStrings } from 'src/config/enums/dbmodels.enum';
 import mongoose, { Model } from 'mongoose';
 import { Category } from './categories.interface';
 import { DBCall } from 'src/utils/calls';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class CategoriesService {
   constructor(
+    private authService: AuthService,
     @Inject(MagicStrings.CATEGORY) private categoryModel: Model<Category>
   ){}
 
   create(createCategoryDto: CreateCategoryDto): Promise<Category> {
     const newCategory = new this.categoryModel(createCategoryDto);
+    newCategory.user_id = this.authService.getUser().sub;
     return newCategory.save();
   }
 
   findAll(): Promise<Category[]> {
-    return DBCall.findAll(this.categoryModel);
+    return DBCall.findAll(this.categoryModel, this.authService.getUser());
   }
 
   findOne(id: mongoose.Types.ObjectId): Promise<Category> {
-    return DBCall.findOne(this.categoryModel, id);
+    return DBCall.findOne(this.categoryModel, id, this.authService.getUser());
   }
 
   update(id: mongoose.Types.ObjectId, updateCategoryDto: UpdateCategoryDto) {
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -5,28 +5,31 @@ import { Task } from './tasks.interface';
 import { MagicStrings } from 'src/config/enums/dbmodels.enum';
 import mongoose, { Model } from 'mongoose';
 import { DBCall } from 'src/utils/calls';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class TasksService {
   constructor(
+    private authService: AuthService,
     @Inject(MagicStrings.TASK) private tasksModel: Model<Task>
   ) {}
 
   create(createTaskDto: CreateTaskDto) {
     const newTask = new this.tasksModel(createTaskDto);
+    newTask.user_id = this.authService.getUser().sub
     return newTask.save();
   }
 
   findAll(): Promise<Task[]> {
-    return DBCall.findAll(this.tasksModel);
+    return DBCall.findAll(this.tasksModel, this.authService.getUser());
   }
 
   findByCategory(id: mongoose.Types.ObjectId) {
-    return this.tasksModel.find({ category: id, isDeleted: false });
+    return DBCall.findAllBy(this.tasksModel, { category: id }, this.authService.getUser());
   }
 
   findOne(id: mongoose.Types.ObjectId): Promise<Task> {
-    return DBCall.findOne(this.tasksModel, id);
+    return DBCall.findOne(this.tasksModel, id, this.authService.getUser());
   }
 
   update(id: mongoose.Types.ObjectId, updateTaskDto: UpdateTaskDto) {
