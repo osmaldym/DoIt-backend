e8a3d889cf7e1f70c3c0f1b667adaa7a0bea86fe
osmaldym <osmaldy11checo@gmail.com>
2024-12-19 03:33:59 -0400

    Stopping passing model as param, instead, using a class variable

--- src/modules/db-call/db-call.service.ts
+++ src/modules/db-call/db-call.service.ts
@@ -1,40 +1,50 @@
 import { Inject, Injectable, Scope } from '@nestjs/common';
-import mongoose, { Model } from "mongoose"
+import mongoose, { Document, Model } from "mongoose"
 import { preventsToShow } from "src/config/prevents";
 import { REQUEST } from '@nestjs/core';
 
+/**
+ * After call in constructor, set the mongo model like:
+ * ```
+ * constructor(dbCall: DbCallService, ...) {
+ *   dbCall.model = *yourModel*;
+ * }
+ * ```
+ */
 @Injectable({ scope: Scope.REQUEST })
 export class DbCallService {
+    model: Model<any>;
+
     constructor(@Inject(REQUEST) private readonly req: Request,) {}
 
     getUserId(): mongoose.Types.ObjectId {
         return this.req['user'].sub;
     }
 
-    async findOne(model: Model<any>, id: mongoose.Types.ObjectId, byAuth: boolean = true) {
+    async findOne(id: mongoose.Types.ObjectId, byAuth: boolean = true) {
         let byUserOpt = {};
         if (byAuth) byUserOpt['user_id'] = this.getUserId();
-        return await model.findOne({ _id: id, deleted: false, ...byUserOpt }).select(preventsToShow);
+        return await this.model.findOne({ _id: id, deleted: false, ...byUserOpt }).select(preventsToShow);
     }
 
-    async findAll(model: Model<any>, byAuth: boolean = true) {
+    async findAll(byAuth: boolean = true) {
         let byUserOpt = {}
         if (byAuth) byUserOpt['user_id'] = this.getUserId();
-        return await model.find({ deleted: false, ...byUserOpt }).select(preventsToShow).exec();
+        return await this.model.find({ deleted: false, ...byUserOpt }).select(preventsToShow).exec();
     }
 
-    async findAllBy(model: Model<any>, filter: any, byAuth: boolean = true) {
+    async findAllBy(filter: any, byAuth: boolean = true) {
         let byUserOpt = {};
         if (byAuth) byUserOpt['user_id'] = this.getUserId();
-        return await model.find({ deleted: false, ...filter, ...byUserOpt }).select(preventsToShow).exec();
+        return await this.model.find({ deleted: false, ...filter, ...byUserOpt }).select(preventsToShow).exec();
     }
 
-    updateOne(model: Model<any>, id: mongoose.Types.ObjectId, updateData: any){
-        return model.updateOne({ _id: id, deleted: false }, updateData);
+    updateOne(id: mongoose.Types.ObjectId, updateData: any){
+        return this.model.updateOne({ _id: id, deleted: false }, updateData);
     }
 
-    async softRemoveOne(model: Model<any>, id: mongoose.Types.ObjectId) {
-        const updatedRes = await model.updateOne({ _id: id, deleted: false }, { deleted: true, deletedAt: new Date() });
+    async softRemoveOne(id: mongoose.Types.ObjectId) {
+        const updatedRes = await this.model.updateOne({ _id: id, deleted: false }, { deleted: true, deletedAt: new Date() });
         const res: mongoose.mongo.DeleteResult = {
             acknowledged: updatedRes.acknowledged,
             deletedCount: updatedRes.modifiedCount,
