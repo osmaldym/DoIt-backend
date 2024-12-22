4efdf4353e3d8a6f46a76dc11709f7e9c73cbef9
osmaldym <osmaldy11checo@gmail.com>
2024-12-22 15:00:03 -0400

    Adding new validation for password min length

--- src/modules/auth/dto/signIn.dto.ts
+++ src/modules/auth/dto/signIn.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+const STR_MIN_LEN: number = 8;
 
 export class SignInDTO {
     @IsEmail()
     readonly email: string;
 
     @IsNotEmpty()
+    @MinLength(STR_MIN_LEN)
     readonly password: string;
 }
