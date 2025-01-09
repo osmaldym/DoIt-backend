c7ba7494a3cbfb65b736894179861a850be0942b
osmaldym <osmaldy11checo@gmail.com>
2025-01-08 20:18:28 -0400

    Updating schema renaming title to name

--- src/schemas/category.schema.ts
+++ src/schemas/category.schema.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose'
 import { SoftDelete } from './softdelete.schema'
 
 export const CategorySchema = new mongoose.Schema({
-    title: String,
+    name: String,
     user_id: mongoose.Types.ObjectId,
     by_user: Boolean,
     ...SoftDelete
