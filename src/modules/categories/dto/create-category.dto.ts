5548d69dc98a6bbc4680fa522d33dbcd5a8ca628
osmaldym <osmaldy11checo@gmail.com>
2025-01-10 03:08:41 -0400

    Adding a max length to categories names

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,6 +1,9 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, MaxLength } from "class-validator";
+
+const STR_MAX_LEN: number = 20;
 
 export class CreateCategoryDto {
     @IsNotEmpty()
+    @MaxLength(STR_MAX_LEN)
     readonly name: string;
 }
